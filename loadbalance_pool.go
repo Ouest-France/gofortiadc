2e63a55f9cee5337f90b08132e53eb595d137d93
Pablo RUTH <contact@pablo-ruth.fr>
2019-03-21 14:36:51 +0100

    update pool calls for api 5.1

--- loadbalance_pool.go
+++ loadbalance_pool.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 )
 
 // LoadbalancePoolReq represents a real server pool request
@@ -32,17 +31,23 @@ type LoadbalancePoolRes struct {
 
 // LoadbalanceGetPools returns the list of all real server pools
 func (c *Client) LoadbalanceGetPools() ([]LoadbalancePoolRes, error) {
-	get, err := c.Client.Get(fmt.Sprintf("%s/api/load_balance_pool", c.Address))
+
+	req, err := c.NewRequest("GET", fmt.Sprintf("%s/api/load_balance_pool", c.Address), nil)
 	if err != nil {
 		return []LoadbalancePoolRes{}, err
 	}
-	defer get.Body.Close()
 
-	if get.StatusCode != 200 {
-		return []LoadbalancePoolRes{}, fmt.Errorf("failed to get pools list with status code: %d", get.StatusCode)
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return []LoadbalancePoolRes{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return []LoadbalancePoolRes{}, fmt.Errorf("failed to get pools list with status code: %d", res.StatusCode)
 	}
 
-	body, err := ioutil.ReadAll(get.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return []LoadbalancePoolRes{}, err
 	}
@@ -60,17 +65,23 @@ func (c *Client) LoadbalanceGetPools() ([]LoadbalancePoolRes, error) {
 
 // LoadbalanceGetPool returns a real server pool by name
 func (c *Client) LoadbalanceGetPool(name string) (LoadbalancePoolRes, error) {
-	get, err := c.Client.Get(fmt.Sprintf("%s/api/load_balance_pool", c.Address))
+
+	req, err := c.NewRequest("GET", fmt.Sprintf("%s/api/load_balance_pool", c.Address), nil)
 	if err != nil {
 		return LoadbalancePoolRes{}, err
 	}
-	defer get.Body.Close()
 
-	if get.StatusCode != 200 {
-		return LoadbalancePoolRes{}, fmt.Errorf("failed to get pool with status code: %d", get.StatusCode)
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return LoadbalancePoolRes{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return LoadbalancePoolRes{}, fmt.Errorf("failed to get pool with status code: %d", res.StatusCode)
 	}
 
-	body, err := ioutil.ReadAll(get.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return LoadbalancePoolRes{}, err
 	}
@@ -93,36 +104,41 @@ func (c *Client) LoadbalanceGetPool(name string) (LoadbalancePoolRes, error) {
 }
 
 // LoadbalanceCreatePool creates a new real server pool
-func (c *Client) LoadbalanceCreatePool(req LoadbalancePoolReq) error {
+func (c *Client) LoadbalanceCreatePool(pool LoadbalancePoolReq) error {
 
-	payloadJSON, err := json.Marshal(req)
+	payloadJSON, err := json.Marshal(pool)
 	if err != nil {
 		return err
 	}
 
-	resp, err := c.Client.Post(fmt.Sprintf("%s/api/load_balance_pool", c.Address), "application/json", bytes.NewReader(payloadJSON))
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("virtual server pool creation failed with status code: %d", resp.StatusCode)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
+	req, err := c.NewRequest("POST", fmt.Sprintf("%s/api/load_balance_pool", c.Address), bytes.NewReader(payloadJSON))
 	if err != nil {
 		return err
 	}
 
-	res := struct{ Payload int }{}
-	err = json.Unmarshal(body, &res)
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("virtual server pool creation failed with status code: %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return err
 	}
 
-	if res.Payload != 0 {
-		return fmt.Errorf("virtual server pool creation failed: %s", getErrorMessage(res.Payload))
+	resJSON := struct{ Payload int }{}
+	err = json.Unmarshal(body, &resJSON)
+	if err != nil {
+		return err
+	}
+
+	if resJSON.Payload != 0 {
+		return fmt.Errorf("virtual server pool creation failed: %s", getErrorMessage(resJSON.Payload))
 	}
 
 	return nil
@@ -136,35 +152,35 @@ func (c *Client) LoadbalanceUpdatePool(pool LoadbalancePoolReq) error {
 		return err
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/api/load_balance_pool/%s", c.Address, pool.Mkey), bytes.NewReader(payloadJSON))
+	req, err := c.NewRequest("PUT", fmt.Sprintf("%s/api/load_balance_pool?mkey=%s", c.Address, pool.Mkey), bytes.NewReader(payloadJSON))
 	if err != nil {
 		return err
 	}
 
-	resp, err := c.Client.Do(req)
+	res, err := c.Client.Do(req)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer res.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("pool update failed with status code: %d", resp.StatusCode)
+	if res.StatusCode != 200 {
+		return fmt.Errorf("pool update failed with status code: %d", res.StatusCode)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return err
 	}
 	fmt.Println(string(body))
 
-	res := struct{ Payload int }{}
-	err = json.Unmarshal(body, &res)
+	resJSON := struct{ Payload int }{}
+	err = json.Unmarshal(body, &resJSON)
 	if err != nil {
 		return err
 	}
 
-	if res.Payload != 0 {
-		return fmt.Errorf("pool update failed: %s", getErrorMessage(res.Payload))
+	if resJSON.Payload != 0 {
+		return fmt.Errorf("pool update failed: %s", getErrorMessage(resJSON.Payload))
 	}
 
 	return nil
@@ -177,34 +193,34 @@ func (c *Client) LoadbalanceDeletePool(pool string) error {
 		return errors.New("pool name cannot be empty")
 	}
 
-	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/api/load_balance_pool/%s", c.Address, pool), nil)
+	req, err := c.NewRequest("DELETE", fmt.Sprintf("%s/api/load_balance_pool?mkey=%s", c.Address, pool), nil)
 	if err != nil {
 		return err
 	}
 
-	resp, err := c.Client.Do(req)
+	res, err := c.Client.Do(req)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer res.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("deletion failed with status code: %d", resp.StatusCode)
+	if res.StatusCode != 200 {
+		return fmt.Errorf("deletion failed with status code: %d", res.StatusCode)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	res := struct{ Payload int }{}
-	err = json.Unmarshal(body, &res)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return err
 	}
 
-	if res.Payload != 0 {
-		return fmt.Errorf("deletion failed: %s", getErrorMessage(res.Payload))
+	resJSON := struct{ Payload int }{}
+	err = json.Unmarshal(body, &resJSON)
+	if err != nil {
+		return err
+	}
+
+	if resJSON.Payload != 0 {
+		return fmt.Errorf("deletion failed: %s", getErrorMessage(resJSON.Payload))
 	}
 
 	return nil
