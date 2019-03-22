a29a7d361b83a7e331745492f82fed8bd6ccabd0
Pablo RUTH <contact@pablo-ruth.fr>
2019-03-22 15:17:54 +0100

    add local certificates api

---
+++ system_certificate_local.go
@@ -0,0 +1,194 @@
+package gofortiadc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+)
+
+// SystemLocalCertificate represents a real server request/response
+type SystemLocalCertificate struct {
+	CaType      string        `json:"ca_type"`
+	Comments    string        `json:"comments"`
+	Extension   []interface{} `json:"extension"`
+	Fingerprint string        `json:"fingerprint"`
+	Hash        string        `json:"hash"`
+	Issuer      string        `json:"issuer"`
+	Mkey        string        `json:"mkey"`
+	PinSha256   string        `json:"pin-sha256"`
+	Sn          string        `json:"sn"`
+	Status      string        `json:"status"`
+	Subject     string        `json:"subject"`
+	Type        string        `json:"type"`
+	Validfrom   string        `json:"validfrom"`
+	Validto     string        `json:"validto"`
+	Version     int           `json:"version"`
+}
+
+// SystemGetLocalCertificates returns the list of all local certificates
+func (c *Client) SystemGetLocalCertificates() ([]SystemLocalCertificate, error) {
+	req, err := c.NewRequest("GET", fmt.Sprintf("%s/api/system_certificate_local", c.Address), nil)
+	if err != nil {
+		return []SystemLocalCertificate{}, err
+	}
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return []SystemLocalCertificate{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return []SystemLocalCertificate{}, fmt.Errorf("failed to get local certificates list with status code: %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return []SystemLocalCertificate{}, err
+	}
+
+	var SystemLocalCertificatePayload struct {
+		Payload []SystemLocalCertificate
+	}
+	err = json.Unmarshal(body, &SystemLocalCertificatePayload)
+	if err != nil {
+		return []SystemLocalCertificate{}, err
+	}
+
+	return SystemLocalCertificatePayload.Payload, nil
+}
+
+// SystemGetLocalCertificate returns a real server by name
+func (c *Client) SystemGetLocalCertificate(name string) (SystemLocalCertificate, error) {
+
+	certificates, err := c.SystemGetLocalCertificates()
+	if err != nil {
+		return SystemLocalCertificate{}, err
+	}
+
+	for _, certificate := range certificates {
+		if certificate.Mkey == name {
+			return certificate, nil
+		}
+	}
+
+	return SystemLocalCertificate{}, fmt.Errorf("local certificate %s not found", name)
+}
+
+// SystemCreateLocalCertificate creates a new local certificate
+func (c *Client) SystemCreateLocalCertificate(name, password string, cert, key []byte) error {
+
+	form, contentType, err := createCertificateForm(name, password, cert, key)
+	if err != nil {
+		return err
+	}
+
+	req, err := c.NewRequest("POST", fmt.Sprintf("%s/api/upload/certificate_local", c.Address), form)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", contentType)
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("local certificate creation failed with status code: %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	resJSON := struct{ Payload int }{}
+	err = json.Unmarshal(body, &resJSON)
+	if err != nil {
+		return err
+	}
+
+	if resJSON.Payload != 0 {
+		return fmt.Errorf("local certificate creation failed: %s ", getErrorMessage(resJSON.Payload))
+	}
+
+	return nil
+}
+
+func createCertificateForm(name, password string, cert, key []byte) (form *bytes.Buffer, contentType string, err error) {
+
+	var b bytes.Buffer
+
+	w := multipart.NewWriter(&b)
+	defer w.Close()
+
+	w.WriteField("mkey", name)
+	w.WriteField("vdom", "global")
+	w.WriteField("type", "CertKey")
+	w.WriteField("passwd", password)
+
+	certPart, err := w.CreateFormFile("cert", "tls.crt")
+	if err != nil {
+		return &b, contentType, err
+	}
+
+	_, err = certPart.Write(cert)
+	if err != nil {
+		return &b, contentType, err
+	}
+
+	keyPart, err := w.CreateFormFile("key", "tls.key")
+	if err != nil {
+		return &b, contentType, err
+	}
+
+	_, err = keyPart.Write(key)
+
+	return &b, w.FormDataContentType(), err
+}
+
+// SystemDeleteLocalCertificate deletes an existing local certificate
+func (c *Client) SystemDeleteLocalCertificate(name string) error {
+
+	if len(name) == 0 {
+		return errors.New("local certificate name cannot be empty")
+	}
+
+	req, err := c.NewRequest("DELETE", fmt.Sprintf("%s/api/system_certificate_local?mkey=%s", c.Address, name), nil)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("local certificate deletion failed with status code: %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	resJSON := struct{ Payload int }{}
+	err = json.Unmarshal(body, &resJSON)
+	if err != nil {
+		return err
+	}
+
+	if resJSON.Payload != 0 {
+		return fmt.Errorf("local certificate deletion failed: %s", getErrorMessage(resJSON.Payload))
+	}
+
+	return nil
+}
---
+++ system_certificate_local_test.go
@@ -0,0 +1,111 @@
+package gofortiadc
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestClient_SystemGetLocalCertificates(t *testing.T) {
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := client.SystemGetLocalCertificates()
+	if err == nil {
+		t.Logf("%+v", res)
+		t.Fatal(err)
+	}
+}
+
+func TestClient_SystemGetLocalCertificate(t *testing.T) {
+	if os.Getenv("TEST_LENS") != "true" {
+		t.Skip()
+	}
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = client.SystemGetLocalCertificate("Factory")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_SystemCreateLocalCertificate(t *testing.T) {
+	if os.Getenv("TEST_LENS") != "true" {
+		t.Skip()
+	}
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cert, key, err := generateCertificate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.SystemCreateLocalCertificate("gofortiadc", "", cert, key)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_SystemDeleteLocalCertificate(t *testing.T) {
+	if os.Getenv("TEST_LENS") != "true" {
+		t.Skip()
+	}
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.SystemDeleteLocalCertificate("gofortiadc")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func generateCertificate() (cert, key []byte, err error) {
+
+	rawKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return cert, key, err
+	}
+
+	template := x509.Certificate{
+		Subject: pkix.Name{
+			Organization: []string{"Gofortiadc Test"},
+		},
+		SerialNumber:          big.NewInt(1),
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(time.Minute * 30),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		DNSNames:              []string{"gofortiadc.test.tld"},
+	}
+
+	rawCert, err := x509.CreateCertificate(rand.Reader, &template, &template, &rawKey.PublicKey, rawKey)
+	if err != nil {
+		return cert, key, err
+	}
+
+	cert = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: rawCert})
+	key = pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(rawKey)})
+
+	return cert, key, nil
+}
