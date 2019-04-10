f23875c1515ad55b72bdeb809429aad833f831dc
Pablo RUTH <contact@pablo-ruth.fr>
2019-04-10 17:50:59 +0200

    add system_certificate_local_group apis

---
+++ system_certificate_local_group_test.go
@@ -0,0 +1,72 @@
+package gofortiadc
+
+import (
+	"os"
+	"testing"
+)
+
+func TestClient_SystemGetLocalCertificateGroups(t *testing.T) {
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := client.SystemGetLocalCertificateGroups()
+	if err != nil {
+		t.Logf("%+v", res)
+		t.Fatal(err)
+	}
+}
+
+func TestClient_SystemGetLocalCertificateGroup(t *testing.T) {
+	if os.Getenv("TEST_LENS") != "true" {
+		t.Skip()
+	}
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = client.SystemGetLocalCertificateGroup("goforti_group")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_SystemCreateLocalCertificateGroup(t *testing.T) {
+	if os.Getenv("TEST_LENS") != "true" {
+		t.Skip()
+	}
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	group := SystemLocalCertificateGroup{
+		Mkey: "goforti_group",
+	}
+
+	err = client.SystemCreateLocalCertificateGroup(group)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_SystemDeleteLocalCertificateGroup(t *testing.T) {
+	if os.Getenv("TEST_LENS") != "true" {
+		t.Skip()
+	}
+
+	client, err := NewClientHelper()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.SystemDeleteLocalCertificateGroup("goforti_group")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
