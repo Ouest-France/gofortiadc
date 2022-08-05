4c2ee3417e0844b9dd660351a962659644c28dcb
Tyler Marshall <108074780+tmarshall98@users.noreply.github.com>
2022-08-05 08:17:32 -0400

    Add VDom parameter to Client, add as query parameter to request urls. (#15)
    * Add VDom parameter to Client, add as query parameter to request urls.
    
    * add TestClient_NewRequest to test that new vdom parameter doesn't break previous behaviour
    
    * add mkey test
    
    Co-authored-by: Pablo RUTH <contact@pablo-ruth.fr>

---
+++ client_test.go
@@ -0,0 +1,86 @@
+package gofortiadc
+
+import (
+	"testing"
+)
+
+// TestClient_NewRequest creates an http.Request with authorization header set
+func TestClient_NewRequest(t *testing.T) {
+
+	// Create Table test case
+	type testCase struct {
+		method string
+		url    string
+		host   string
+		path   string
+		vdom   string
+	}
+
+	// Create test cases
+	testCases := []testCase{
+		{
+			method: "GET",
+			url:    "http://localhost:8080/api/v1/status/system",
+			host:   "localhost:8080",
+			path:   "/api/v1/status/system",
+			vdom:   "",
+		},
+		{
+			method: "POST",
+			url:    "http://localhost:8080/api/v1/status/system",
+			host:   "localhost:8080",
+			path:   "/api/v1/status/system",
+			vdom:   "root",
+		},
+		{
+			method: "POST",
+			url:    "http://localhost:8080/api/load_balance_virtual_server?mkey=test",
+			host:   "localhost:8080",
+			path:   "/api/load_balance_virtual_server",
+			vdom:   "root",
+		},
+	}
+	// Iterate over test cases
+	for _, tc := range testCases {
+
+		// Create client
+		client, err := NewClientHelper()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Set vdom parameter on client
+		client.VDom = tc.vdom
+
+		// Create request
+		req, err := client.NewRequest(tc.method, tc.url, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Test if req.Host is set with the correct value
+		if req.Host != tc.host {
+			t.Errorf("req.Host is not set with the correct value, expected %s, got %s", tc.host, req.Host)
+		}
+
+		// Test if req.Path is set with the correct value
+		if req.URL.Path != tc.path {
+			t.Errorf("req.Path is not set with the correct value, expected: %s, got: %s", tc.path, req.URL.Path)
+		}
+
+		// Test if authorization header is set with the correct value
+		if req.Header.Get("Authorization") != "Bearer "+client.Token {
+			t.Errorf("Authorization header is not set")
+		}
+
+		// Test if vdom parameter is set when vdom is set on client
+		if tc.vdom != "" && req.URL.Query().Get("vdom") != tc.vdom {
+			t.Errorf("vdom parameter is not set")
+		}
+
+		// Test if vdom parameter is not set when vdom is not set on client
+		if tc.vdom == "" && req.URL.Query().Get("vdom") != "" {
+			t.Errorf("vdom parameter is set")
+		}
+	}
+}
